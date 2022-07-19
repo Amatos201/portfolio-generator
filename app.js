0a10fc772128567245286cd2dc9831cfe82f1766
amatos <amatos201@gmail.com>
2022-07-19 01:30:54 -0400

    added template

--- app.js
+++ app.js
@@ -124,11 +124,18 @@ const promptProject = portfolioData =>{
         }
       });
   };
-    
- promptUser()
- .then(promptProject)
- .then(portfolioData => {
-console.log(portfolioData);
+
+    promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+     const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
+
  });
  
   
